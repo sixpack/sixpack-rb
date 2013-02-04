becd1e5524f13d9958a78f7c34174234ff7dd062
Mike Dirolf <mike@dirolf.com>
2013-02-04 16:27:31 -0500

    generate_client_id and some error checking.

--- lib/sixpack.rb
+++ lib/sixpack.rb
@@ -1,21 +1,25 @@
 require "net/http"
 require "json"
 
+require "uuid"
+
 require "sixpack/version"
 
 module Sixpack
-  def self.simple_participate(experiment_name, alternatives, options={})
-    default_options = {:client_id => nil, :force => nil}
-    options = default_options.merge(options)
-
-    session = Session.new(options[:client_id])
-    res = session.participate(experiment_name, alternatives, options[:force])
+  def self.simple_participate(experiment_name, alternatives, client_id, force=nil)
+    session = Session.new(client_id)
+    res = session.participate(experiment_name, alternatives, force)
     res["alternative"]
   end
 
   def self.simple_convert(experiment_name, client_id=nil)
     session = Session.new(client_id)
     session.convert(experiment_name)["status"]
+  end
+
+  def self.generate_client_id
+    uuid = UUID.new
+    uuid.generate
   end
 
   class Session
@@ -28,6 +32,10 @@ module Sixpack
     end
 
     def participate(experiment_name, alternatives, force=nil)
+      if !(experiment_name =~ /[a-z0-9][a-z0-9\-_ ]*/)
+        raise ArgumentError, "Bad experiment_name"
+      end
+
       params = {
         :client_id => @client_id,
         :experiment => experiment_name,
--- spec/lib/sixpack_spec.rb
+++ spec/lib/sixpack_spec.rb
@@ -2,12 +2,17 @@ require 'spec_helper'
 
 describe Sixpack do
   it "should return an alternative for simple_participate" do
-    alternative = Sixpack.simple_participate('show-bieber', ['trolled', 'not-trolled'], :client_id => "mike")
+    alternative = Sixpack.simple_participate('show-bieber', ['trolled', 'not-trolled'], "mike")
+    ['trolled', 'not-trolled'].should include(alternative)
+  end
+
+  it "should auto generate a client_id" do
+    alternative = Sixpack.simple_participate('show-bieber', ['trolled', 'not-trolled'], nil)
     ['trolled', 'not-trolled'].should include(alternative)
   end
 
   it "should return ok for simple_convert" do
-    alternative = Sixpack.simple_participate('show-bieber', ['trolled', 'not-trolled'], :client_id => "mike")
+    alternative = Sixpack.simple_participate('show-bieber', ['trolled', 'not-trolled'], "mike")
     Sixpack.simple_convert("show-bieber", "mike").should == "ok"
   end
 
@@ -16,7 +21,13 @@ describe Sixpack do
   end
 
   it "should not return ok for simple_convert with new experiment" do
-    alternative = Sixpack.simple_participate('show-bieber', ['trolled', 'not-trolled'], :client_id => "mike")
+    alternative = Sixpack.simple_participate('show-bieber', ['trolled', 'not-trolled'], "mike")
     Sixpack.simple_convert("show-blieber", "mike").should == "failed"
+  end
+
+  it "should not allow bad experiment names" do
+    expect {
+      Sixpack.simple_participate('%%', ['trolled', 'not-trolled'], nil)
+    }.to raise_error
   end
 end

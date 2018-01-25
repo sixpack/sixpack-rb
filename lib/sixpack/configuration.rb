bfa347dd1c8b3d6acb9200df80d5120e4f92dff4
Roopesh Jupudi <roopesh.jupudi@harmoney.co.nz>
2018-01-25 14:06:43 +1300

    Configuring read timeout

--- lib/sixpack/configuration.rb
+++ lib/sixpack/configuration.rb
@@ -1,13 +1,14 @@
 module Sixpack
   class Configuration
-    attr_accessor :base_url, :user, :password
+    attr_accessor :base_url, :user, :password, :read_timeout
 
     def initialize
       @base_url = 'http://localhost:5000'
+      @read_timeout = 1.0
     end
 
     def to_hash
-      config = {base_url: @base_url}
+      config = { base_url: @base_url, read_timeout: @read_timeout }
       config[:user] = @user unless @user.nil?
       config[:password] = @password unless @password.nil?
       config
